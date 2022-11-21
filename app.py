b30879e8d5fbd52db2ebea5633c0e3aa8865f4ac
Jan Zysko <janzysko@gmail.com>
2022-11-21 21:24:00 +0100

    appending move times to pgn and writing to a new file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from itertools import pairwise
+import itertools
 import logging
 import os
 
@@ -6,6 +6,7 @@ from flask import Flask, flash, request, redirect, render_template, url_for
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
+from exceptions_ import MissingValueException
 from forms import GameForm, MoveForm
 from loaders import pgn_game_generator
 from config import config
@@ -15,6 +16,12 @@ root = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 root.setLevel(logging.INFO)
 logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
+
+
+def pairwise_longest(iterable):
+    """s -> (s0, s1), (s2, s3), (s4, s5), ..."""
+    a = iter(iterable)
+    return itertools.zip_longest(a, a)
 
 
 def create_app(app_environment=None):
@@ -55,24 +62,44 @@ def create_app(app_environment=None):
         if request.method == 'GET':
             with open(f'upload/{request.args.get("filename")}') as pgn_file:
                 for i, game in enumerate(pgn_game_generator(pgn_file)):
-                    if i < int(request.args.get("game_no")):
+                    if i != int(request.args.get("game_no")):
                         continue
                     else:
                         game_form = GameForm()
                         headers = game.headers
                         game_form.title.data = f'{headers["White"]} - {headers["Black"]}, {headers["Date"]}'
-                        for move_no, (move_white, move_black) in enumerate(pairwise(game.mainline())):
+                        for move_no, (move_white, move_black) in enumerate(pairwise_longest(game.mainline())):
                             move_form = MoveForm()
                             move_form.move_number = move_no + 1
                             move_form.white_move = move_white.move
-                            move_form.black_move = move_black.move
-                            move_form.white_time = None
-                            move_form.black_time = None
+                            move_form.white_time = move_white.clock()
+                            try:
+                                move_form.black_move = move_black.move
+                                move_form.black_time = move_black.clock()
+                            except AttributeError:
+                                move_form.black_move = '---'
+                                move_form.black_time = '---'
                             game_form.moves.append_entry(move_form)
-
                         return render_template('table.html', game_form=game_form)
-                else:
-                    return 'cry'
+        elif request.method == 'POST':
+            with open(f'upload/{request.args.get("filename")}') as pgn_file_input:
+                for i, game in enumerate(pgn_game_generator(pgn_file_input)):
+                    if i != int(request.args.get("game_no")):
+                        continue
+                    for move_no, (move_white, move_black) in enumerate(pairwise_longest(game.mainline())):
+                        try:
+                            move_white.set_clock(int(request.form[f'moves-{move_no}-white_time']))
+                            move_black.set_clock(int(request.form[f'moves-{move_no}-black_time']))
+                        except AttributeError:
+                            move_white.set_clock(int(request.form[f'moves-{move_no}-white_time']))
+                        except ValueError:
+                            raise MissingValueException("Missing clock values!")
+                            # TODO: this should be changed to just giving the same page without
+                            # TODO: or actually have a validator on the website
+                    print(game, file=open(f'download/{request.args.get("filename")}', "a"), end="\n\n")
+            return redirect(
+                url_for('game_data', filename=request.args.get("filename"),
+                        game_no=int(request.args.get("game_no")) + 1))
 
     return app
 
